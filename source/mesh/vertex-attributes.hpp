30dbf5232b57703b2d7d81f7e54be16607f16e06
Mahmoud Atwa <m.atwa@hotmail.com>
2020-11-29 15:58:22 +0200

    Fixed Importing

--- source/mesh/vertex-attributes.hpp
+++ source/mesh/vertex-attributes.hpp
@@ -1,12 +1,7 @@
 #ifndef TEARS_ENGINE_VERTEX_ATTRIBUTES_H
 #define TEARS_ENGINE_VERTEX_ATTRIBUTES_H
+#include "../../vendor/glad/include/glad/gl.h"
 
-#include <glad/gl.h>
-
-namespace our {
-
-    // Just as convenience, we write which location "we" use for each attribute in our shader.
-    // If you used different locations, modify these constants or just ignore (dont' use) them
     namespace default_attribute_locations {
         inline constexpr GLuint POSITION = 0;
         inline constexpr GLuint COLOR = 1;
@@ -20,6 +15,5 @@ namespace our {
         // Make sure this is specialized for every type it is called for
         static_assert(sizeof(T) != sizeof(T), "No accessors defined for this type");
     };
-}
 
 #endif
